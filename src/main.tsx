2d7353b0c61a1bccc8f0c889cc22e7576ee35da6
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2023-11-27 13:59:05 +0700

    Completed Navbar UI and Homepage UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store, persistor } from './redux/_stores'
+import { PersistGate } from 'redux-persist/integration/react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  )
+}
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import './Homepage.scss'
 
 function Homepage() {
-  return <div>Homepage</div>
+  return (
+    <div className='homepage'>
+      <div className='homepage__background'>
+        <div className='homepage__content'>
+          <h1>
+            Trường đại học Bách Khoa <br />
+            ĐHQG TP.HCM
+          </h1>
+          <p>Ứng dụng đăng ký đồ án tốt nghiệp</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Homepage
